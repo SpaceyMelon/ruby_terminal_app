32f498aca082a908007ad7b4ae8721adc8f40369
SpaceyMelon <Kasey.Capulet@gmail.com>
2020-12-16 17:44:24 +1100

    created cat_profiles, hard coded profiles to be refactored

---
+++ cat_profiles.rb
@@ -0,0 +1,55 @@
+system "clear"
+        box = TTY::Box.frame(padding: 0, align: :center, border: :thick) do
+            "Cat Profiles"
+        end
+        print box
+box = TTY::Box.frame(padding: 0, align: :center, width: 80, height: 20) do
+
+"Name: Danger\n
+Age: 4\n
+Breed: Domestic short hair (Ginger)\n
+Favourite Toys: Marbles\n
+Favourite Scritch Spots: Under the chin and between the ears\n
+Danger is a fiery boy who loves playing chase and hide and seek!\n 
+He has a bit if an ego though so it's best to let him win to avoid a sulky puss (nothing a scratch under the chin won't fix!)"
+end
+
+print box
+
+box = TTY::Box.frame(padding: 0, align: :center, width: 80, height: 20) do
+
+"Name: Fran\n
+Age: 2\n
+Breed: Domestic long hair (Grey and White)\n
+Favourite Toys: Laser pointer and her jingly strawberry\n
+Favourite Scritch Spots: Top of her head and base of her tail\n
+Fran is a polite and bougie lady that let's her wild side out at the site of a laser pointer!\n 
+As much as she loves to play Fran will just as happily perch close by and allow you to adore her\n
+**Fran is partial to female company**"
+    end
+    print box
+
+box = TTY::Box.frame(padding: 0, align: :center, width: 80, height: 20) do
+
+"Name: Spock\n
+Age: 5\n
+Breed: Domestic Short hair (Black)\n
+Favourite Toys: Anything dangly!\n
+Favourite Scritch Spots: Back scratches\n
+Spock is one energetic lady! She loves chasing and attacking anything you can dangle on the end of a string\n 
+If all the chasing tires you out she'll let you rest if you play a game of fetch"
+    end        
+    print box
+
+    box = TTY::Box.frame(padding: 0, align: :center, width: 80, height: 20) do
+
+"Name: Smokey\n
+Age: 2\n
+Breed: Domestic long hair (Brown)\n
+Favourite Toys: Wind up mouse and tunnels!\n
+Favourite Scritch Spots: Under the chin and the belly\n
+Smokey is a gentle giant. He may look boistrous when it's playtime but just loves a cuddle!\n 
+You'll have a hard time saying no to this guy. He has somehow mastered the kitty version of puppy eyes"
+        end        
+        print box
+
